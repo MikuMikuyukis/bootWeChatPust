bd7ef1020874c8ae67b33fc56dfbc9db665a4edd
MikuMikuyukis <35860554+MikuMikuyukis@users.noreply.github.com>
2020-04-27 20:53:43 +0800

    Update BootWeChatpush.py

--- BootWeChatpush.py
+++ BootWeChatpush.py
@@ -2,6 +2,19 @@ import requests
 import time
 import socket
 import random
+import datetime
+import os
+import sys
+
+def currentTime():
+    now = datetime.datetime.now()
+    month = now.month
+    day = now.day
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+    currenttime = "{}月{}日{}时{}分{}秒".format(month, day, hour, minute, second)
+    return currenttime
 
 
 def getIP():
@@ -15,15 +28,37 @@ def getPcname():
     return pcname
 
 
-def postServerChan(ip, pcname,URL):
-    ran = random.randint(10, 99)
-    dataout = str("主机名:{}设备公网ip地址:{}随机数{}".format(pcname, ip, ran))
+def postServerChan(ip, pcname, times,requestsURL):
+    ran = random.randint(1, 9999)
+    dataout = str("主机名:{},时间：{}设备公网ip地址:{}随机数{},".format(pcname, times, ip, ran))
     data = {
         "text": "主人您的{}已经开机了喵".format(pcname),
         "desp": dataout
     }
-    requests.post("URL", data=data)
+    requests.post(requestsURL, data=data)
 
-URL = ""#填你ServerChanURL
-time.sleep(5)
-postServerChan(getIP(), getPcname(),URL)
+
+def networktest(requestsURL):
+    times = currentTime()
+    while True:
+        pingtest = os.system("ping www.baidu.com -n 1")
+        if pingtest == 0:
+            print("网络正常")
+            postServerChan(getIP(), getPcname(), times,requestsURL)
+            break
+
+        else:
+            print("网络错误正在重试")
+            time.sleep(1)
+def import_par():
+    try:
+        parameter = sys.argv[1]
+        return parameter
+    except IndexError:
+        print("请传入ServerChanURL")
+        exit()
+
+
+if __name__ == "__main__":
+    requestsURL = import_par()
+    networktest(requestsURL)
